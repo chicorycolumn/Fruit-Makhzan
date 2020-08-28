3083eb69554ec96929d6335d5cf6ffda6b8088d5
C <chrismatusemail@gmail.com>
2020-08-28 08:59:58 +0100

    simplifying query as test

--- utils/table_utils.php
+++ utils/table_utils.php
@@ -279,8 +279,7 @@ function make_table(
       PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    echo $table_name;
-    echo $create_table_querystring;
+    echo $query;
     print_r($connection);
     print_r($query_array);
     exit();
