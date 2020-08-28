c5f810f4163a72ba79cf5b0e3cfeb37c4b369688
C <chrismatusemail@gmail.com>
2020-08-28 08:56:58 +0100

    print statements

--- utils/table_utils.php
+++ utils/table_utils.php
@@ -279,6 +279,10 @@ function make_table(
       PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    echo $table_name;
+    echo $create_table_querystring;
+    print_r($connection);
+    print_r($query_array);
     exit();
   }
 
