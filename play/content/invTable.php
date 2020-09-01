c09fe68ea9ba1207f17fc8e79325af76b0c42f85
C <chrismatusemail@gmail.com>
2020-09-01 18:08:56 +0100

    Added tooltips for table headers.

--- play/content/invTable.php
+++ play/content/invTable.php
@@ -2,7 +2,8 @@
 
 echo "<link rel='stylesheet' type='text/css' href='.././css/invTable.css' />";
 
-$invTable = "
+$invTable =
+  "
 <div class='invTable'>
 <div class='row'>
 <div class='col'>
@@ -10,14 +11,25 @@ $invTable = "
   <div class='boxBody'>
     <table id='inventory' class='table'>
       <thead>
-      <tr>
-        <th class='th_inventory'>Name</th>
-        <th class='th_inventory'>Quantity</th>
-        <th class='th_inventory'>Selling price</th>
-        <th class='th_inventory' style='cursor: help;' onClick=printDevDataHTML()>Restock price</th>
-        <th class='th_inventory' style='cursor: help;' onClick=printDevData1()>Factors</th>
-        <th class='th_inventory' style='cursor: help;' onClick=printDevData2()>Restock</th>
-      </tr>
+      <tr>" .
+  "<th class='th_inventory'>Name" .
+  "</th>" .
+  "<th class='th_inventory'>Quantity ⓘ" .
+  "<span class='tooltip'>How many of each fruit you currently have.</span>" .
+  "</th>" .
+  "<th class='th_inventory'>Selling price ⓘ" .
+  "<span class='tooltip'>How much you're selling each fruit for. Click to change it.</span>" .
+  "</th>" .
+  "<th class='th_inventory' onClick=printDevDataHTML()>Restock price ⓘ" .
+  "<span class='tooltip'>The price it will cost to restock each fruit, along with a pin showing its current popularity. A higher popularity means people will buy your fruit it for a higher price, but also raises the price it will cost you to restock.</span>" .
+  "</th>" .
+  "<th class='th_inventory' onClick=printDevData1()>Factors ⓘ" .
+  "<span class='tooltip'>What affects each fruit's popularity. The larger text is the major factor, while the smaller is the minor. Red text with a flipped arrow means that this fruit's popularity is affected negatively by this factor, so `↻weather` in red means this fruit is actually more popular in worse (lower scoring) weather.</span>" .
+  "</th>" .
+  "<th class='th_inventory' onClick=printDevData2()>Restock ⓘ" .
+  "<span class='tooltip'>Buy each fruit at its restock price to increase your inventory. Click buttons or type numbers to specify the quantity you want, then click BUY.</span>" .
+  "</th>" .
+  "</tr>
       </thead>
       <tbody>
       </tbody>
